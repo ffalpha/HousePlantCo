9c52d1a8424a0f6ac8a6c184a3f9dab5604cb8ea
Kalana Mihiranga <kalanam217@gmail.com>
2025-07-11 01:27:09 +1000

    sync

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
@@ -9,8 +9,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+  <HashRouter>
+  <App />
+</HashRouter>
   </Provider>
 );
